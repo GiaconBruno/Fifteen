aea5d9fcd4c0b8f7f97531c4396871c67ab7245e
GiaconBruno <56009821+GiaconBruno@users.noreply.github.com>
2020-04-10 16:52:21 -0300

    att

--- build/precache-manifest.a5628b030c21900f70682a82d256cc9c.js
+++ build/precache-manifest.a5628b030c21900f70682a82d256cc9c.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "10f6b6489d9840a79b1047f6d629dd87",
+    "revision": "dbb895b72ab11ee649c0ae6200afbc98",
     "url": "/index.html"
   },
   {
-    "revision": "8b2e576caea47085cc82",
-    "url": "/static/css/main.e702d0eb.chunk.css"
+    "revision": "35d8c96aa95b70973c5c",
+    "url": "/static/css/main.6dfea878.chunk.css"
   },
   {
     "revision": "c08bc34cac1f2b73cd3e",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.c9c6ec89.chunk.js.LICENSE"
   },
   {
-    "revision": "8b2e576caea47085cc82",
-    "url": "/static/js/main.c17da94f.chunk.js"
+    "revision": "35d8c96aa95b70973c5c",
+    "url": "/static/js/main.2db183e9.chunk.js"
   },
   {
     "revision": "28c994496324b99257d8",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.83d6af928837a979c1962fc9fcbdc131.js"
+  "/precache-manifest.a5628b030c21900f70682a82d256cc9c.js"
 );
 
 self.addEventListener('message', (event) => {
